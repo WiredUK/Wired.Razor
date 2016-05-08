e743259fdc48cfbfd4b2a49cd530f319436fc1a3
David Glass <wireduk@gmail.com>
2016-05-08 12:04:46 +0100

    Add project files.

---
+++ Wired.Razor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wired.Razor
+{
+    public class Class1
+    {
+    }
+}
